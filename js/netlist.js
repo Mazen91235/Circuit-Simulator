b3b33ab4d46960f95aa24d1286248b3484513268
unknown <14712023100209@stud.cu.edu.eg>
2024-11-13 01:24:54 +0200

    Commit3 V1.0

--- js/netlist.js
+++ js/netlist.js
@@ -46,9 +46,9 @@ function CreateNetlist(){
             delete_comp = '<button class="btn btn-danger" onclick="DeleteComponent('+ GetIndex(source) +')"><i class="fa-solid fa-trash"></i></button>';
         }
         if(source["type"] == "V"){
-            sources_container.innerHTML += '<div><div class="voltage component flip-'+ source["heading"] +'"><span>+</span><h4>V</h4><span>-</span></div><h4>V = '+ source["value"] +' '+ source["unit"] +'V</h4><h4>I = '+ AssignUnit(source["I"]) +'A</h4><h4>From '+ source["from"] +' To '+ source["to"] +'</h4><div class="options"><button class="btn btn-warning" onclick="ShowEdit('+ GetIndex(source) +')"><i class="fa-solid fa-pen"></i></button>'+ delete_comp +'</div></div>';
+            sources_container.innerHTML += '<div><div class="voltage component flip-'+ source["heading"] +'"><span>+</span><h4>V</h4><span>-</span></div><h4>V = '+ source["value"] +' '+ source["unit"] +'V</h4><h4>I = '+ AssignUnit(source["I"]) +'A</h4><h4>P = '+ GetPower(source)[1] +'W</h4><h4>From '+ source["from"] +' To '+ source["to"] +'</h4><div class="options"><button class="btn btn-warning" onclick="ShowEdit('+ GetIndex(source) +')"><i class="fa-solid fa-pen"></i></button>'+ delete_comp +'</div></div>';
         }else{
-            sources_container.innerHTML += '<div><div class="current component flip-'+ source["heading"] +'"><i class="fa-solid fa-arrow-up"></i></div><h4>I = '+ source["value"] +' '+ source["unit"] +'A</h4><h4>V = '+ GetV(source["from"],source["to"])[1] +'V</h4><h4>From '+ source["from"] +' To '+ source["to"] +'</h4><div class="options"><button class="btn btn-warning" onclick="ShowEdit('+ GetIndex(source) +')"><i class="fa-solid fa-pen"></i></button>'+ delete_comp +'</div></div></div>';
+            sources_container.innerHTML += '<div><div class="current component flip-'+ source["heading"] +'"><i class="fa-solid fa-arrow-up"></i></div><h4>I = '+ source["value"] +' '+ source["unit"] +'A</h4><h4>V = '+ GetV(source["from"],source["to"])[1] +'V</h4><h4>P = '+ GetPower(source)[1] +'W</h4><h4>From '+ source["from"] +' To '+ source["to"] +'</h4><div class="options"><button class="btn btn-warning" onclick="ShowEdit('+ GetIndex(source) +')"><i class="fa-solid fa-pen"></i></button>'+ delete_comp +'</div></div></div>';
         }
     }
     for(let i=0;i<elements.length;i++){
@@ -56,6 +56,6 @@ function CreateNetlist(){
         if(element != components[1]){
             delete_comp = '<button class="btn btn-danger" onclick="DeleteComponent('+ GetIndex(element) +')"><i class="fa-solid fa-trash"></i></button>';
         }
-        elements_container.innerHTML += '<div><div class="resistance component flip-'+ element["heading"] +'"><h4>+</h4><h4>/\\/\\/\\/\\</h4><h4>-</h4></div><h4>R = '+ element["value"] + ' ' + element["unit"] +'Ω</h4><h4>V = '+ GetV(element["from"],element["to"])[1] +'V</h4><h4>I = '+ GetI(element["from"],element["to"],element["value"],element["unit"])[1] +'A</h4><h4>From '+ element["from"] +' To '+ element["to"] +'</h4><div class="options"><button class="btn btn-warning" onclick="ShowEdit('+ GetIndex(element) +')"><i class="fa-solid fa-pen"></i></button>'+ delete_comp +'</div></div>';
+        elements_container.innerHTML += '<div><div class="resistance component flip-'+ element["heading"] +'"><h4>+</h4><h4>/\\/\\/\\/\\</h4><h4>-</h4></div><h4>R = '+ element["value"] + ' ' + element["unit"] +'Ω</h4><h4>V = '+ GetV(element["from"],element["to"])[1] +'V</h4><h4>I = '+ GetI(element["from"],element["to"],element["value"],element["unit"])[1] +'A</h4><h4>P = '+ GetPower(element)[1] +'W</h4><h4>From '+ element["from"] +' To '+ element["to"] +'</h4><div class="options"><button class="btn btn-warning" onclick="ShowEdit('+ GetIndex(element) +')"><i class="fa-solid fa-pen"></i></button>'+ delete_comp +'</div></div>';
     }
 }
